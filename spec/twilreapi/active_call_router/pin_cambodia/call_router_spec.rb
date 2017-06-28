a75d14dc62aa051defb29bc63b0753b2cf4c7355
David Wilkie <dwilkie@gmail.com>
2017-06-28 13:45:05 +0700

    Update routing instructions for MT through Smart

--- spec/twilreapi/active_call_router/pin_cambodia/call_router_spec.rb
+++ spec/twilreapi/active_call_router/pin_cambodia/call_router_spec.rb
@@ -94,8 +94,10 @@ describe Twilreapi::ActiveCallRouter::PinCambodia::CallRouter do
 
       context "Smart" do
         let(:destination) { smart_number }
-        let(:asserted_gateway) { "pin_kh_07" }
-        let(:asserted_address) { "010344566" }
+        let(:asserted_host) { "27.109.112.80" }
+        let(:asserted_address) { "010344566@#{asserted_host}" }
+        let(:asserted_dial_string_path) { "external/#{asserted_address}" }
+
         it { assert_routing_instructions! }
       end
 
